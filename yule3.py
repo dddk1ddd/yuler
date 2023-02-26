bc650fc850c7b316ff744a3e10cb79258dea6c83
David Ricci <121657626+dddk1ddd@users.noreply.github.com>
2023-02-25 21:14:59 -0500

    Add files via upload
    added timer args

--- yule3.py
+++ yule3.py
@@ -4,25 +4,26 @@ from bs4 import BeautifulSoup
 from mechanize import Browser
 from urllib.error import HTTPError
 import time
+import argparse
 
 url_list = []
 url_listb = []
 
 try:
     with open('goodlist.txt', 'r') as f:
-        url_list = f.read().splitlines()
+        url_list = [line.strip() for line in f]
     with open('badlist.txt', 'r') as f:
-        url_listb = f.read().splitlines()
+        url_listb = [line.strip() for line in f]
 except FileNotFoundError:
     pass
 
 fire = False
 cdown = 0
+minmax = []
 
 
 def save_files():
-    print("Writing files...")
-    global url_list, url_listb
+    print("\n Writing files goodlist.txt and badlist.txt")
     with open('goodlist.txt', 'w') as f:
         f.write('\n'.join(url_list))
     with open('badlist.txt', 'w') as f:
@@ -30,45 +31,76 @@ def save_files():
     return
 
 
-def do_links():
-    global cdown, url_list, url_listb
-    cdown = cdown + 1
-    # generate random youtube link
-    x = ''.join([random.choice(string.ascii_letters + string.digits + "_-") for n in range(11)])
-    while x in url_list or x in url_listb:
-        print("Generated Duplicate Link; Re-generating")
-        x = ''.join([random.choice(string.ascii_letters + string.digits + "_-") for n in range(11)])
+def get_video_info(video_url):
     br = Browser()
     try:
-        res = br.open("http://www.youtube.com/watch?v=" + x)
+        res = br.open(video_url)
         data = res.get_data()
         soup = BeautifulSoup(data, "lxml")
-        title = soup.find('title')
-        # bad links are titled ' - YouTube'
-        # good links have other titles
-        if str(title) != "<title> - YouTube</title>":
-            print("Title: " + str(title))
-            url_list.append("http://www.youtube.com/watch?v=" + x)
-            print("CHECK -> http://www.youtube.com/watch?v=" + x)
-        else:
-            url_listb.append("http://www.youtube.com/watch?v=" + x)
-
+        title = soup.find('title').text
+        status_tag = soup.find('meta', {'itemprop': 'unlisted'})
+        status = True if status_tag is not None else False
+        if status:
+            print("\n Title: " + title)
+            print(" Video unlisted: " + str(status))
+        return title, status
     except HTTPError:
-        print("Error ")
-        print("ERROR at:: http://www.youtube.com/watch?v=" + x)
+        print("\n* Error ")
+        print("* ERROR at:: " + video_url)
+        return None, None
 
-    print("TRY: " + str(cdown) + "\nGOOD LINKS: " + str(len(url_list)) + "\nBAD LINKS: " + str(len(url_listb)))
-    sleep_time = random.randint(5, 30)
-    print("Sleeping " + str(sleep_time) + " seconds...")
-    time.sleep(sleep_time)
-    do_links()
+
+def do_links(minmax):
+    global cdown, url_list, url_listb
+    cdown += 1
+    # generate random youtube link
+    video_id = ''.join([random.choice(string.ascii_letters + string.digits + "_-") for n in range(11)])
+    video_url = "http://www.youtube.com/watch?v=" + video_id
+    while video_url in url_list or video_url in url_listb:
+        print("[ Generated Duplicate Link; Re-generating ]")
+        video_id = ''.join([random.choice(string.ascii_letters + string.digits + "_-") for n in range(11)])
+        video_url = "http://www.youtube.com/watch?v=" + video_id
+
+    title, status = get_video_info(video_url)
+    if status:
+        url_list.append(str(video_url) + " Unlisted: " + str(status))
+        print(" GOOD URL -> " + video_url)
+    else:
+        url_listb.append(video_url)
+        print(" BAD URL  -> " + video_url)
+
+    print("\n   TRY: " + str(cdown) +
+          "\n   GOOD LINKS: " + str(len(url_list)) +
+          "\n   BAD LINKS: " + str(len(url_listb)) + "\n")
+    if minmax != [0, 0]:
+        sleep_time = random.randint(minmax[0], minmax[1])
+        print("Waiting " + str(sleep_time) + " seconds...")
+        time.sleep(sleep_time)
+    do_links(minmax)
 
 
 def main():
-    print("Good urls generated with this script so far:")
-    print(url_list)
+    parser = argparse.ArgumentParser(
+        prog='yule3.py',
+        description='Scrape YouTube for random links and check if unlisted.',
+        epilog='www.phatkid.art')
+    parser.add_argument('--timer', nargs=2, type=int, default=[0, 0], help='timer min max in seconds')
+    args = parser.parse_args()
+    global minmax
+    if args.timer != [0, 0]:
+        try:
+            if args.timer[1] >= args.timer[0]:
+                minmax = [args.timer[0], args.timer[1]]
+            else:
+                minmax = [args.timer[1], args.timer[0]]
+        except:
+            return
+    else:
+        minmax = [0, 0]
+    print("Timer set to min max " + str(minmax))
+    print("* goodlist.txt:" + str(url_list) + "\n")
     try:
-        do_links()
+        do_links(minmax)
     except KeyboardInterrupt:
         save_files()
 
